d4e6ef66d6754468189989f8c9c8348073c576d9
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-04 21:24:09 +0500

    Task no 21

---
+++ index.ts
@@ -0,0 +1,11 @@
+         /// Task # 21 ///
+//They think of something you could store in a TypeScript Object. 
+//Write a program that creates Objects containing these items.
+
+let car: {model: string; Company: string; yearofimoprt: number} = {
+    model: "Ghq365",
+    Company: "Honda",
+    yearofimoprt: 2022
+}
+console.log(`Car information: ${car.model} by ${car.Company} import in ${car.yearofimoprt}`)
+
